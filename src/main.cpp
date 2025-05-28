f631358b1469d62fae23c4273c69a89f4de8d220
dukesook <devonsookhoo14@gmail.com>
2025-05-28 15:24:09 -0600

    remove 3rd party from main.cp

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,8 @@
 #include "main_args.h" // Custom header for argument parsing
 #include "raw2gimi.h"
 #include <iostream>
-#include <libheif/heif.h> // built from source
-#include <libheif/heif_experimental.h>
-#include <libheif/heif_properties.h>
-#include <libheif/heif_tai_timestamps.h>
-// #include <libraw/libraw.h> // sudo apt install libraw-dev
 
 using namespace std;
-
-static void print_versions() {
-  // cout << "LibRaw version: " << LibRaw::version() << endl;
-  cout << "LibHEIF version: " << heif_get_version() << endl;
-}
 
 bool execute_action(string action, MainArgs args) {
   if (action == "write_image_from_memory" || action.empty()) {
@@ -28,8 +18,7 @@ bool execute_action(string action, MainArgs args) {
 }
 
 int main(int argc, const char *argv[]) {
-
-  print_versions();
+  cout << "================= raw2gimi =================" << endl;
 
   // Parse Options
   MainArgs args(argc, argv);
