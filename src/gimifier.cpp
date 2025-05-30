80f1bb5671be4f7f0885f7b61f9e3dec083ee174
dukesook <devonsookhoo14@gmail.com>
2025-05-30 13:53:04 -0600

    todo: Gimifier::write_to_file(video)

--- src/gimifier.cpp
+++ src/gimifier.cpp
@@ -7,3 +7,8 @@ using namespace gimi;
 void Gimifier::write_to_file(const RawImage &image, WriteOptions options) {
   LibheifWrapper::write_to_heif(image, options);
 }
+
+void Gimifier::write_to_file(vector<RawImage> &, WriteOptions) {
+  cout << "TODO: write_to_file(vector<RawImage> &)" << endl;
+  exit(1);
+}
--- src/gimifier.h
+++ src/gimifier.h
@@ -3,14 +3,19 @@
 #include "model/raw_image.h"
 #include "model/write_options.h"
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 namespace gimi {
+
+// The Gimifier class writes GIMI files.
 class Gimifier {
 public:
   static void write_to_file(const RawImage &, WriteOptions);
+  static void write_to_file(vector<RawImage> &, WriteOptions);
 
 protected:
 };
+
 } // namespace gimi
