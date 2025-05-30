92c5c4b9fd82429456c49b5ee60a65a561b7911a
dukesook <devonsookhoo14@gmail.com>
2025-05-30 11:18:10 -0600

    new file: write_options.cpp

---
+++ src/model/write_options.cpp
@@ -0,0 +1,10 @@
+#include "model/write_options.h"
+
+using namespace gimi;
+
+WriteOptions::WriteOptions() {
+  output_filename = "output.heif";
+  chroma = Chroma::rgb;
+  interleave = Interleave::interleaved;
+  codec = Codec::raw;
+}
