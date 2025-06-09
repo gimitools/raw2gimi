73b77691af0801660f48117320c8bf44538e4b58
dukesook <devonsookhoo14@gmail.com>
2025-06-09 12:16:38 -0600

    WIP: Call heif_context_add_grid_image()

--- src/dependencies/libheif_wrapper.cpp
+++ src/dependencies/libheif_wrapper.cpp
@@ -46,8 +46,41 @@ void LibheifWrapper::add_image(const RawImage &rawImage) {
   gimify(primary_id);
 }
 
-void LibheifWrapper::add_grid(const vector<RawImage> &tiles) {
-  throw_error("Function not yet implemented");
+void LibheifWrapper::add_grid(const vector<RawImage> &rawImages) {
+  vector<heif_image *> tiles;
+  heif_image *img;
+  heif_image_handle *handle;
+  heif_encoder *encoder;
+  heif_compression_format compression = extract_compression(m_options.codec);
+  heif_chroma chroma = extract_chroma(rawImages[0]);
+  heif_colorspace colorspace = extract_colorspace(rawImages[0]);
+
+  // Convert Tiles
+  for (const RawImage &rawImage : rawImages) {
+    img = convert_to_heif_image(rawImage, colorspace, chroma);
+    tiles.push_back(img);
+  }
+
+  // Get Encoder
+  he(heif_context_get_encoder_for_format(m_ctx, compression, &encoder));
+
+  uint32_t image_width;
+  uint32_t image_height;
+  uint32_t tile_columns;
+  uint32_t tile_rows;
+  heif_encoding_options *encoding_options = nullptr;
+  heif_image_handle *out_grid_image_handle;
+  heif_context_add_grid_image(
+      m_ctx,
+      image_width,
+      image_height,
+      tile_columns,
+      tile_rows,
+      encoding_options,
+      &out_grid_image_handle);
+
+  heif_item_id primary_id = heif_image_handle_get_item_id(handle);
+  gimify(primary_id);
 }
 
 void LibheifWrapper::write_to_heif() {
