cbe949c74135f0dff5c611f9c9316d791304cffa
dukesook <devonsookhoo14@gmail.com>
2025-07-05 08:46:08 -0600

    WIP: Streamline the MainArgsBuilder

--- src/main_args_generator.cpp
+++ src/main_args_generator.cpp
@@ -96,16 +96,23 @@ void MainArgsGenerator::add_args_grid(vector<MainArgs> &v) {
 }
 
 void MainArgsGenerator::add_args_sequence(vector<MainArgs> &v) {
-  MainArgs sequence;
+  MainArgs args;
   MainArgsBuilder builder;
+  // Defaults
   builder.action("create_sequence")
-      .codec("hevc")
       .chroma("rgb")
       .interleave("planar")
-      .pixel_type("8")
-      .output_filename("out/hevc_sequence_64x64.mp4");
-  sequence = std::move(builder.build());
-  v.push_back(sequence);
+      .pixel_type("8");
+
+  args = builder.codec("hevc")
+             .output_filename("out/hevc_sequence_64x64.mp4")
+             .build();
+  v.push_back(args);
+
+  args = builder.codec("av1")
+             .output_filename("out/av1_sequence_64x64.mp4")
+             .build();
+  v.push_back(args);
 }
 
 string MainArgsGenerator::create_filename(MainArgs p) {
