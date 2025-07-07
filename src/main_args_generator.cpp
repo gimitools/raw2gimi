bf60dba80bfe400397ae464498328e09c39da378
dukesook <devonsookhoo14@gmail.com>
2025-07-07 13:52:18 -0600

    add_args_simple_av1()

--- src/main_args_generator.cpp
+++ src/main_args_generator.cpp
@@ -109,45 +109,59 @@ void MainArgsGenerator::add_args_simple_uncompressed(vector<MainArgs> &v) {
   string width = "64";
   string height = "64";
 
-  // Uncompressed
+  // RGB Interleaved
   v.push_back(create_args("unc", "rgb", "interleaved", "8", height, width));
   v.push_back(create_args("unc", "rgb", "interleaved", "10", height, width));
   v.push_back(create_args("unc", "rgb", "interleaved", "12", height, width));
   v.push_back(create_args("unc", "rgb", "interleaved", "14", height, width));
   v.push_back(create_args("unc", "rgb", "interleaved", "16", height, width));
 
+  // RGB Planar
   v.push_back(create_args("unc", "rgb", "planar", "8", height, width));
   // v.push_back(create_args("unc", "rgb", "planar", "10", height, width));
   // v.push_back(create_args("unc", "rgb", "planar", "12", height, width));
   // v.push_back(create_args("unc", "rgb", "planar", "14", height, width));
   // v.push_back(create_args("unc", "rgb", "planar", "16", height, width));
 
+  // Monochrome
   v.push_back(create_args("unc", "mono", "", "8", height, width));
   v.push_back(create_args("unc", "mono", "", "10", height, width));
   v.push_back(create_args("unc", "mono", "", "12", height, width));
   v.push_back(create_args("unc", "mono", "", "14", height, width));
   v.push_back(create_args("unc", "mono", "", "16", height, width));
+
+  // YUV
 }
 
 void MainArgsGenerator::add_args_simple_hevc(vector<MainArgs> &v) {
   string width = "64";
   string height = "64";
 
-  v.push_back(create_args("hevc", "rgb", "interleaved", "8", height, width));
-  v.push_back(create_args("hevc", "mono", "interleaved", "8", height, width));
-  // v.push_back(create_args("hevc", "mono", "interleaved", "10", height, width));
-  // v.push_back(create_args("hevc", "mono", "interleaved", "12", height, width));
-  // v.push_back(create_args("hevc", "mono", "interleaved", "14", height, width));
-  // v.push_back(create_args("hevc", "mono", "interleaved", "16", height, width));
-
+  // RGB Interleaved
   v.push_back(create_args("hevc", "rgb", "interleaved", "8", height, width));
   v.push_back(create_args("hevc", "rgb", "interleaved", "10", height, width));
   v.push_back(create_args("hevc", "rgb", "interleaved", "12", height, width));
   // v.push_back(create_args("hevc", "rgb", "interleaved", "14", height, width));
   // v.push_back(create_args("hevc", "rgb", "interleaved", "16", height, width));
 
-  v.push_back(create_args("hevc", "rgb", "interleaved", "8", height, width));
+  // RGB Planar
   v.push_back(create_args("hevc", "rgb", "planar", "8", height, width));
+  // v.push_back(create_args("hevc", "rgb", "planar", "10", height, width));
+  // v.push_back(create_args("hevc", "rgb", "planar", "12", height, width));
+  // v.push_back(create_args("hevc", "rgb", "planar", "14", height, width));
+  // v.push_back(create_args("hevc", "rgb", "planar", "16", height, width));
+
+  // Monochrome
+  v.push_back(create_args("hevc", "mono", "", "8", height, width));
+  // v.push_back(create_args("hevc", "mono", "", "10", height, width));
+  // v.push_back(create_args("hevc", "mono", "", "12", height, width));
+  // v.push_back(create_args("hevc", "mono", "", "14", height, width));
+  // v.push_back(create_args("hevc", "mono", "", "16", height, width));
+
+  // YUV
+  // v.push_back(create_args("hevc", "444", "interleaved", "8", height, width));
+  // v.push_back(create_args("hevc", "422", "interleaved", "8", height, width));
+  // v.push_back(create_args("hevc", "420", "interleaved", "8", height, width));
   v.push_back(create_args("hevc", "444", "planar", "8", height, width));
   v.push_back(create_args("hevc", "422", "planar", "8", height, width));
   v.push_back(create_args("hevc", "420", "planar", "8", height, width));
@@ -155,7 +169,35 @@ void MainArgsGenerator::add_args_simple_hevc(vector<MainArgs> &v) {
 void MainArgsGenerator::add_args_simple_av1(vector<MainArgs> &v) {
   string width = "64";
   string height = "64";
+
+  // RGB Interleaved
   v.push_back(create_args("av1", "rgb", "interleaved", "8", height, width));
+  v.push_back(create_args("av1", "rgb", "interleaved", "10", height, width));
+  v.push_back(create_args("av1", "rgb", "interleaved", "12", height, width));
+  // v.push_back(create_args("av1", "rgb", "interleaved", "14", height, width));
+  // v.push_back(create_args("av1", "rgb", "interleaved", "16", height, width));
+
+  // RGB Planar
+  v.push_back(create_args("av1", "rgb", "planar", "8", height, width));
+  // v.push_back(create_args("av1", "rgb", "planar", "10", height, width));
+  // v.push_back(create_args("av1", "rgb", "planar", "12", height, width));
+  // v.push_back(create_args("av1", "rgb", "planar", "14", height, width));
+  // v.push_back(create_args("av1", "rgb", "planar", "16", height, width));
+
+  // Monochrome
+  v.push_back(create_args("av1", "mono", "", "8", height, width));
+  // v.push_back(create_args("av1", "mono", "", "10", height, width));
+  // v.push_back(create_args("av1", "mono", "", "12", height, width));
+  // v.push_back(create_args("av1", "mono", "", "14", height, width));
+  // v.push_back(create_args("av1", "mono", "", "16", height, width));
+
+  // YUV
+  // v.push_back(create_args("av1", "444", "interleaved", "8", height, width));
+  // v.push_back(create_args("av1", "422", "interleaved", "8", height, width));
+  // v.push_back(create_args("av1", "420", "interleaved", "8", height, width));
+  v.push_back(create_args("av1", "444", "planar", "8", height, width));
+  v.push_back(create_args("av1", "422", "planar", "8", height, width));
+  v.push_back(create_args("av1", "420", "planar", "8", height, width));
 }
 void MainArgsGenerator::add_args_simple_j2k(vector<MainArgs> &v) {
   // Warning! OpenJPEG not compiled in!
